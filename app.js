f0cdf70f9e4bc4c1440ee6ed0b7356a04ba21c62
NandhiniKrishnamoorthy <nandhiniprasath25@gmail.com>
2021-09-14 09:26:06 -0700

    added textarea

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var buttontrans = document.querySelector("#btntranslate");
 
+var txtinputarea = document.querySelector("#txtarea");
+
+
 buttontrans.addEventListener("click", function clickEvent() {
     console.log("Clicked!");
+    console.log(txtinputarea.value);
 })
