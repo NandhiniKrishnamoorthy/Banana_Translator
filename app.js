d7729e6a02d75a444c553e0ba72a97f85fdd9e18
NandhiniKrishnamoorthy <nandhiniprasath25@gmail.com>
2021-09-14 09:45:39 -0700

    added output textarea

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ var buttontrans = document.querySelector("#btntranslate");
 
 var txtinputarea = document.querySelector("#txtarea");
 
+var outputtextarea = document.querySelector("#outputtext");
+
+
 
 buttontrans.addEventListener("click", function clickEvent() {
     console.log("Clicked!");
-    console.log(txtinputarea.value);
+    //console.log(txtinputarea.value);
+    outputtextarea.innerText = "Welcome Good morning";
 })
