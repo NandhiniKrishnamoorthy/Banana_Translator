19752f1fcf3cbab541e96bbd72b440daa8e3fd59
NandhiniKrishnamoorthy <nandhiniprasath25@gmail.com>
2021-09-15 11:48:16 -0700

    refactor: code changes in html

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 var buttontrans = document.querySelector("#btntranslate");
-
 var txtinputarea = document.querySelector("#txtarea");
-
 var outputtextarea = document.querySelector("#outputtext");
 
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function findurl(inputtext) {
+    var url = serverURL + "?" + "text=" + inputtext;
+    return url;
+}
 
 
-buttontrans.addEventListener("click", function clickEvent() {
-    console.log("Clicked!");
-    //console.log(txtinputarea.value);
-    outputtextarea.innerText = "Welcome Good morning";
+
+buttontrans.addEventListener("click", function clickEvent() {    
+    text = txtinputarea.value;
+    fetch(findurl(text))
+        .then(response => response.json())
+        .then(json => {
+            outputtextarea.innerText = json;
+        })
 })
